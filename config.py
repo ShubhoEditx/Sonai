a2f0043b7a12db2b38da60ac8d1bafe77021de9c
ShubhoEditx <166376289+ShubhoEditx@users.noreply.github.com>
2024-05-08 11:59:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "6951964535"))
+OWNER_ID = int(getenv("OWNER_ID", "1981973428"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -31,15 +31,15 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
-    "UPSTREAM_REPO", "https://github.com/ShubhoEditx/GAMusic",
+    "UPSTREAM_REPO", "https://github.com/ShubhoEditx/Sonai",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/crazzy07_network")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Crazzy_music_07")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_shubho")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BongFamilyWaitingArea")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
