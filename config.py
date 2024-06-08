14f908de1b46b637c18902fa4d5fba0f9c75d975
init_shubho <166376289+ShubhoEditx@users.noreply.github.com>
2024-06-08 11:44:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,6 @@ API_HASH = getenv("API_HASH", "")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
@@ -22,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", "1981973428"))
+OWNER_ID = int(getenv("OWNER_ID", "7146599240"))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
