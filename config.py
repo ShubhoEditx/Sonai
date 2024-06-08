ad61e4f71b647ac40f8eea5ce514b2c052fa5b55
init_shubho <166376289+ShubhoEditx@users.noreply.github.com>
2024-06-08 11:32:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_shubho")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BongFamilyWaitingArea")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/shubho_creations")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
